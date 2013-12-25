c79ef2003d27d8a06e94310f0bcf51a5b545131a
hai <hailxl@gmail.com>
2013-12-25 23:01:51 +0800

    add factor multi kata

--- kata/spp/README.md
+++ kata/spp/README.md
@@ -1,1 +1,3 @@
 Simple Programming Problems
+
+these program is from http://adriann.github.io/programming_problems.html?utm_campaign=Manong_Weekly_Issue_13&utm_medium=EDM&utm_source=Manong_Weekly
--- kata/spp/factor_multi.py
+++ kata/spp/factor_multi.py
@@ -2,4 +2,19 @@
 #encoding: UTF-8
 __author__ = 'bl02515'
 
+import functools
 
+
+def factor(number):
+    return functools.reduce(lambda a, b: a * b, xrange(1, number + 1))
+
+num = raw_input("please enter a number: ")
+while True:
+    try:
+        if 'q' == num:
+            break
+        print('the factor multi of {0} is {1}'.format(num.strip(), factor(int(num.strip()))))
+        num = raw_input("please enter a number: ")
+    except:
+        print("you've enter an incorrect nubmer")
+        num = raw_input("please enter a number: ")
