38ae2037d9ca1d9a30753c8da4fc52c654e7dd0c
hailxl <hailxl@gmail.com>
2014-11-03 22:38:02 +0800

    a stack implemention

---
+++ algorithms/src/main/java/me/changhai/algorithms/ch1/ResizingArrayStack.java
@@ -0,0 +1,22 @@
+package me.changhai.algorithms.ch1;
+
+/**
+ * Created by bl02515 on 14/11/3.
+ */
+public class ResizingArrayStack {
+    public boolean isEmpty(){
+        return false;
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public void push(int item) {
+
+    }
+
+    public int pop() {
+        return 0;
+    }
+}
